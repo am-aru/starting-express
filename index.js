1db70f1e4c7f7f69e24e78babf93ff10ca12bb20
Arpita Sharma <arpita.arusharma@gmail.com>
2024-09-05 13:58:20 +0530

    CRUD methods in express

--- index.js
+++ index.js
@@ -16,7 +16,35 @@ app.post('/data' , (req , res ) =>{
     res.send("data recevied");
 });
 
+const users =[
+    {id:1 , name : "user1"},
+    {id:2 , name : "user2"}
+]
 
+app.post("/users" , (req, res) =>{
+    const newUser = req.body;
+    const userId = users.length + 1;
+    newUser.Id = userId;
+
+    users.push(newUser);
+    res.status(201).json({message : "user created successfully" , user : newUser});
+})
+
+app.get('/users' , (req , res ) =>{
+    res.status(200).json({data : users});
+});
+
+app.delete("/users/:id" , (req, res) =>{
+    const userId = req.params.id;
+    console.log("user Id" , userId);
+
+    const userIndex = users.findIndex((user) => user.id == userId);
+    if(userIndex == -1){
+        return res.status(404).json({message: "user not found"});
+    } 
+    users.splice(userIndex , 1);
+    res.json({message: "user deleted successfully"})
+})
 
 const port = 3000;
  app.listen(port , ()=>{
